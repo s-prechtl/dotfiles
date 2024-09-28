e89d25366b08a93e3e87102e0d3b1d2ddc198747
s-prechtl <stefan@tague.at>
2024-09-28 15:02:31 +0200

    feat: dunst basic gruvbox styling

--- modules/home-manager/dunst.nix
+++ modules/home-manager/dunst.nix
@@ -3,7 +3,18 @@
     enable = true;
     settings = {
       global = {
-        frame_color = "#ffebdbb2";
+        frame_color = "#ebdbb2";
+        background = "#282828";
+        foreground = "#ebdbb2";
+        offset = "30x30";
+        corner_radius = 10;
+        icon_corner_radius = 5;
+        gap_size = 5;
+        font = "JetBrainsMono Nerd Font Mono 10";
+      };
+
+      urgency_critical = {
+        frame_color = "#BF616A";
       };
     };
   };
