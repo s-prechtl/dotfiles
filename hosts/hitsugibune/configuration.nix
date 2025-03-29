642c18a7b6433d22bd85e6ac2d835a6131295f7f
s-prechtl <stefan@tague.at>
2025-03-29 18:35:28 +0100

    feat: minecraft server (trust me 2)

--- hosts/hitsugibune/configuration.nix
+++ hosts/hitsugibune/configuration.nix
@@ -38,41 +38,43 @@ in {
     # This is mandatory, sorry.
     eula = true;
 
-    # The name will be used for the state folder and system user.
-    # In this case, the folder is `/var/lib/mc-aged`
-    # and the user `mc-aged`.
-    aged = {
-      enable = true;
-      jvmMaxAllocation = "12G";
-      jvmInitialAllocation = "2G";
-      jvmOpts = concatStringsSep " " [
-        "-XX:+UseG1GC"
-        "-XX:+ParallelRefProcEnabled"
-        "-XX:MaxGCPauseMillis=200"
-        "-XX:+UnlockExperimentalVMOptions"
-        "-XX:+DisableExplicitGC"
-        "-XX:+AlwaysPreTouch"
-        "-XX:G1NewSizePercent=40"
-        "-XX:G1MaxNewSizePercent=50"
-        "-XX:G1HeapRegionSize=16M"
-        "-XX:G1ReservePercent=15"
-        "-XX:G1HeapWastePercent=5"
-        "-XX:G1MixedGCCountTarget=4"
-        "-XX:InitiatingHeapOccupancyPercent=20"
-        "-XX:G1MixedGCLiveThresholdPercent=90"
-        "-XX:G1RSetUpdatingPauseTimePercent=5"
-        "-XX:SurvivorRatio=32"
-        "-XX:+PerfDisableSharedMem"
-        "-XX:MaxTenuringThreshold=1"
-      ];
+    instances = {
+      # The name will be used for the state folder and system user.
+      # In this case, the folder is `/var/lib/mc-aged`
+      # and the user `mc-aged`.
+      aged = {
+        enable = true;
+        jvmMaxAllocation = "12G";
+        jvmInitialAllocation = "2G";
+        jvmOpts = concatStringsSep " " [
+          "-XX:+UseG1GC"
+          "-XX:+ParallelRefProcEnabled"
+          "-XX:MaxGCPauseMillis=200"
+          "-XX:+UnlockExperimentalVMOptions"
+          "-XX:+DisableExplicitGC"
+          "-XX:+AlwaysPreTouch"
+          "-XX:G1NewSizePercent=40"
+          "-XX:G1MaxNewSizePercent=50"
+          "-XX:G1HeapRegionSize=16M"
+          "-XX:G1ReservePercent=15"
+          "-XX:G1HeapWastePercent=5"
+          "-XX:G1MixedGCCountTarget=4"
+          "-XX:InitiatingHeapOccupancyPercent=20"
+          "-XX:G1MixedGCLiveThresholdPercent=90"
+          "-XX:G1RSetUpdatingPauseTimePercent=5"
+          "-XX:SurvivorRatio=32"
+          "-XX:+PerfDisableSharedMem"
+          "-XX:MaxTenuringThreshold=1"
+        ];
 
-      serverConfig = {
-        server-port = 25566;
-        motd = "SIB24 Aged Server";
-        white-list = true;
-        spawn-protection = 0;
-        max-tick-time = 5 * 60 * 1000;
-        allow-flight = true;
+        serverConfig = {
+          server-port = 25566;
+          motd = "SIB24 Aged Server";
+          white-list = true;
+          spawn-protection = 0;
+          max-tick-time = 5 * 60 * 1000;
+          allow-flight = true;
+        };
       };
     };
   };
--- modules/home-manager/fastfetch.nix
+++ modules/home-manager/fastfetch.nix
@@ -5,29 +5,29 @@
       logo = {
         type = "data";
         source = ''
-                  ;dd,
-                 xdoo'
-                0oooo'
-              ,ko:.oo' .
-             xdo. .oo':od0
-            0oo   .oo';oook,
-          'koc    .oo';oo,odx
-         oxo'     .oo';oo  oo0
-        Ooo       .oo';oo   ;oxc
-      'Ool        .oo';oo    .od0
-     odo'         .oo';oo      cok,
-    0oo.          .oo';oo       'odx
-  .Ooc            .oo';oo         lo0.
- cxo,              cl ;oo          ;oxc
-kdo.   'XXXXXXXXXXX0K.;oo lXXXXXXXX0oodO
- ooOd             .oo,;oo
-   loOx    x0O 00o.oo';oo x0O    d00,
-     cok0         .oo';oo      kOoo
-       :oxxxxxxxxxdoo',oo oxxxxoc
-        'ooooooooooo.  '   :ooo,
-          '';
+                            ;dd,
+                           xdoo'
+                          0oooo'
+                        ,ko:.oo' .
+                       xdo. .oo':od0
+                      0oo   .oo';oook,
+                    'koc    .oo';oo,odx
+                   oxo'     .oo';oo  oo0
+                  Ooo       .oo';oo   ;oxc
+                'Ool        .oo';oo    .od0
+               odo'         .oo';oo      cok,
+              0oo.          .oo';oo       'odx
+            .Ooc            .oo';oo         lo0.
+           cxo,              cl ;oo          ;oxc
+          kdo.   'XXXXXXXXXXX0K.;oo lXXXXXXXX0oodO
+           ooOd             .oo,;oo
+             loOx    x0O 00o.oo';oo x0O    d00,
+               cok0         .oo';oo      kOoo
+                 :oxxxxxxxxxdoo',oo oxxxxoc
+                  'ooooooooooo.  '   :ooo,
+        '';
       };
-    modules = [
+      modules = [
         "os"
         "host"
         "kernel"
