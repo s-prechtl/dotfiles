168b38b49ddfdd9b1f74601b06e0dbf28f055d38
s-prechtl <stefan@tague.at>
2025-03-31 14:15:59 +0200

    feat: docker for pi hole

--- hosts/karasumaru/configuration.nix
+++ hosts/karasumaru/configuration.nix
@@ -6,7 +6,9 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  serverIP = 0.0 .0 .0;
+in {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -32,6 +34,33 @@
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  virtualisation.docker.enable = true;
+  virtualisation.oci-containers = {
+    backend = "docker";
+    virtualisation.oci-containers.containers.pihole = {
+      image = "pihole/pihole:latest";
+      ports = [
+        "${serverIP}:53:53/tcp"
+        "${serverIP}:53:53/udp"
+        "3080:80"
+        "30443:443"
+      ];
+      volumes = [
+        "/var/lib/pihole/:/etc/pihole/"
+        "/var/lib/dnsmasq.d:/etc/dnsmasq.d/"
+      ];
+      environment = {
+        ServerIP = serverIP;
+      };
+      extraOptions = [
+        "--cap-add=NET_ADMIN"
+        "--dns=127.0.0.1"
+        "--dns=1.1.1.1"
+      ];
+      workdir = "/var/lib/pihole/";
     };
   };
 
