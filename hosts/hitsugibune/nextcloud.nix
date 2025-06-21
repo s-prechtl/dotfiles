1340ee934dec7ce1fb624fb568270e9b2f07d647
s-prechtl <stefan@tague.at>
2025-06-21 22:41:12 +0200

    feat: nextcloud updates

--- hosts/hitsugibune/nextcloud.nix
+++ hosts/hitsugibune/nextcloud.nix
@@ -1,10 +1,14 @@
 {config, ...}: {
   # This is only a temporary password and will be changed
-  environment.etc."nextcloud-admin-pass".text = "samc";
+  environment.etc."nextcloud-admin-pass".text = "samcsamc11";
   services.nextcloud = {
     enable = true;
     hostName = "sprechtl.ddns.net";
     https = true;
+    config = {
+      adminpassFile = "/etc/nextcloud-admin-pass";
+      dbtype = "sqlite";
+    };
   };
 
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
