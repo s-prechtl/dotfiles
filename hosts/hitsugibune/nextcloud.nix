d9e3a1aeb16ae977512125b88f4aff7faf259bae
s-prechtl <stefan@tague.at>
2025-06-21 22:42:16 +0200

    feat: nextcloud version

--- hosts/hitsugibune/nextcloud.nix
+++ hosts/hitsugibune/nextcloud.nix
@@ -1,10 +1,11 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   # This is only a temporary password and will be changed
   environment.etc."nextcloud-admin-pass".text = "samcsamc11";
   services.nextcloud = {
     enable = true;
     hostName = "sprechtl.ddns.net";
     https = true;
+    package = pkgs.nextcloud31;
     config = {
       adminpassFile = "/etc/nextcloud-admin-pass";
       dbtype = "sqlite";
