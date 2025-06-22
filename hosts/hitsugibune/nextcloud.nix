674a4bfce542a11b5a43c91a706b73815f29ac51
s-prechtl <stefan@tague.at>
2025-06-22 17:14:12 +0200

    fix: trust

--- hosts/hitsugibune/nextcloud.nix
+++ hosts/hitsugibune/nextcloud.nix
@@ -60,9 +60,8 @@
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-NginX-Proxy true;
             proxy_set_header X-Forwarded-Proto http;
-            #proxy_pass http://127.0.0.1:8080/; # tailing / is important!
             rewrite ^/nextcloud(.*)$ $1 break;
-            proxy_pass http://127.0.0.1:8080
+            proxy_pass http://127.0.0.1:8080/; # tailing / is important!
             proxy_set_header Host $host;
             proxy_cache_bypass $http_upgrade;
             proxy_redirect off;
