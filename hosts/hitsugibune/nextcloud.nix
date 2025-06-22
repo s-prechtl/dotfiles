140e07863e79b0adcc81b14e7fdcfba57e4d99de
s-prechtl <stefan@tague.at>
2025-06-22 17:09:15 +0200

    fix: internet masters may thy solution be worketh

--- hosts/hitsugibune/nextcloud.nix
+++ hosts/hitsugibune/nextcloud.nix
@@ -60,7 +60,9 @@
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-NginX-Proxy true;
             proxy_set_header X-Forwarded-Proto http;
-            proxy_pass http://127.0.0.1:8080/; # tailing / is important!
+            #proxy_pass http://127.0.0.1:8080/; # tailing / is important!
+            rewrite ^/nextcloud(.*)$ $1 break;
+            proxy_pass http://localhost:8080
             proxy_set_header Host $host;
             proxy_cache_bypass $http_upgrade;
             proxy_redirect off;
