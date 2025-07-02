fca9e91a39e97f5ef89ddf194436ba841295a30d
s-prechtl <stefan@tague.at>
2025-07-02 12:12:09 +0200

    feat: what is this bluetooth blyat

--- hosts/goingmerry/configuration.nix
+++ hosts/goingmerry/configuration.nix
@@ -19,6 +19,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
   networking.hostName = "goingmerry"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
