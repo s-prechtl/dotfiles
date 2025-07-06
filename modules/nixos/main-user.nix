aeb3e4b244ff79f9658c97d379cb932f7f7563ce
s-prechtl <stefan@tague.at>
2025-07-06 15:44:33 +0200

    feat: kvm group

--- modules/nixos/main-user.nix
+++ modules/nixos/main-user.nix
@@ -23,7 +23,7 @@ in {
     users.users.${cfg.username} = {
       isNormalUser = true;
       initialPassword = "12345";
-      extraGroups = ["docker" "input" "networkmanager" "wheel" "vboxusers" "libvirtd" "wireshark"];
+      extraGroups = ["docker" "input" "networkmanager" "wheel" "vboxusers" "libvirtd" "wireshark" "kvm"];
       description = "Stefan";
       shell = pkgs.zsh;
     };
