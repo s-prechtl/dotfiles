bd92cf4a2bfb047131126391999b9f01b87baea3
s-prechtl <stefan@tague.at>
2025-09-29 11:14:07 +0200

    feat: disk spindown eliminated

---
+++ hosts/saberofxebec/disk-spindown.nix
@@ -0,0 +1,14 @@
+{lib, pkgs, ...}: {
+# Disables spindown on all disks of /dev/sd* format. -S might be used later not sure if needed yet.
+services.udev.extraRules = 
+  let
+    mkRule = as: lib.concatStringsSep ", " as;
+    mkRules = rs: lib.concatStringsSep "\n" rs;
+  in mkRules ([( mkRule [
+    ''ACTION=="add|change"''
+    ''SUBSYSTEM=="block"''
+    ''KERNEL=="sd[a-z]"''
+    ''ATTR{queue/rotational}=="1"''
+    ''RUN+="${pkgs.hdparm}/bin/hdparm -B 254 /dev/%k"''
+  ])]);
+}
