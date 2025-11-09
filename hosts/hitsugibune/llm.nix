55aa108d6b2293a092eeec317983ac2a95af4e13
s-prechtl <stefan@tague.at>
2025-11-09 16:40:12 +0100

    feat: jetzt reichts

--- hosts/hitsugibune/llm.nix
+++ hosts/hitsugibune/llm.nix
@@ -1,8 +1,8 @@
-{config, ...}: {
+{...}: {
   services.open-webui = {
     enable = true;
     openFirewall = true;
-    host = "chattn.sprechtl.me";
+    host = "0.0.0.0";
   };
 
   services.ollama = {
@@ -13,18 +13,12 @@
 
   services.nginx = {
     enable = true;
-    virtualHosts.${config.services.open-webui.host} = {
+    virtualHosts."chattn.sprechtl.me" = {
       forceSSL = true;
       enableACME = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:8080";
         proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-        '';
       };
     };
   };
