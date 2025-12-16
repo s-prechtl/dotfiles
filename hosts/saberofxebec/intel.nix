57095702147b4abe010eec46cec7717e262bdcdc
s-prechtl <stefan@tague.at>
2025-12-16 19:01:09 +0100

    feat: disable qsv?

--- hosts/saberofxebec/intel.nix
+++ hosts/saberofxebec/intel.nix
@@ -8,7 +8,6 @@
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver # Enable Hardware Acceleration
-      vpl-gpu-rt # Enable QSV
     ];
   };
   environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
